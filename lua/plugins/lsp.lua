6fa45a4939f1e1199b69c73129f0ecdc781f5ea4
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-07 11:16:22 +0100

    feat(lsp): added example for custom lsp server setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,13 @@ return {
   -- uncomment and add lsp servers with their config to servers below
   {
     "neovim/nvim-lspconfig",
+    -- you can do any additional lsp server setup here
+    -- return true if you don't want this server to be setup with lspconfig
+    ---@param server string lsp server name
+    ---@param opts _.lspconfig.options any options set for the server
+    -- setup_server = function(server, opts)
+    --   return false
+    -- end,
     ---@type lspconfig.options
     -- servers = {
     --   jsonls = {},
