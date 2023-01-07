e8d9f84e54387c58e197da445fdbad3d20c57f01
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-07 11:31:55 +0100

    docs: comment all examples

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -6,26 +6,26 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- change trouble config
-  {
-    "folke/trouble.nvim",
-    -- config = { use_diagnostic_signs = true },
-  },
+  -- {
+  --   "folke/trouble.nvim",
+  --   config = { use_diagnostic_signs = true },
+  -- },
 
   -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = true,
-  },
+  -- {
+  --   "simrat39/symbols-outline.nvim",
+  --   cmd = "SymbolsOutline",
+  --   keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+  --   config = true,
+  -- },
 
   -- add zen-mode
-  {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    config = true,
-    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-  },
+  -- {
+  --   "folke/zen-mode.nvim",
+  --   cmd = "ZenMode",
+  --   config = true,
+  --   keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  -- },
 
   -- use mini.starter instead of alpha
   -- { "goolord/alpha-nvim", enabled = false },
