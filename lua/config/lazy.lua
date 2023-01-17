e1f0f3443d2e44ded226b5188bd8ece708ee8fba
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-17 16:39:38 +0100

    feat: set `config.defaults.version` for now. Too many semver plugins have outdated releases

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,10 @@ require("lazy").setup({
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    version = "*", -- try installing the latest stable version for plugins that support semver
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
