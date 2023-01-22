ad809a2885d6a8fb13f43148a4380f5621299fde
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-22 21:26:08 +0100

    fix(lazy): dont disable matchit, matchparen and netrw by default

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,9 +32,9 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
