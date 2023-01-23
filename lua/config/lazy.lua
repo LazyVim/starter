9a49b2d0310a3557d4294f6a11ec28b1ee41d384
hunterliao29 <hunterliao29@gmail.com>
2023-01-23 16:05:04 -0500

    rearrange imports order so that user can overwrite extra modules

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,12 +10,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
