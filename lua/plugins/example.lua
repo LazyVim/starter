01a27d15c4c66f0d8626f06d295a05a9a86a1d2f
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-24 14:50:26 +0100

    refactor: simplified fzf native example

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -74,14 +74,14 @@ return {
 
   -- add telescope-fzf-native
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-    -- apply the config and additionally load fzf-native
-    config = function(_, opts)
-      local telescope = require("telescope")
-      telescope.setup(opts)
-      telescope.load_extension("fzf")
-    end,
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
   },
 
   -- add pyright to lspconfig
