d9aa1d0f08d2b9b29765132b401175da125031e8
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-28 12:22:52 +0200

    style: typo

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -242,7 +242,7 @@ return {
           if cmp.visible() then
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
+            -- this way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
