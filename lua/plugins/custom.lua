dd4cc070d18ccfcc0bec5b2024ddba786b2e6f58
Mateusz Nowakowski <mat13mn@gmail.com>
2023-08-10 18:12:14 +0200

    disable folke/flash for 's' key

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -10,7 +10,7 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash", },
+      { "s", mode = { "n", "x", "o" }, "s", desc = "", },
       { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search", },
