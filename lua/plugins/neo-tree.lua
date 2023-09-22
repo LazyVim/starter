5db3074d5dc06efd69cf2b0cb101911f6462ca74
smith.hu <smith.hu@keeptruckin.com>
2023-09-22 08:19:28 +0800

    [change] add colorscheme and neo-tree lua

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
+}
