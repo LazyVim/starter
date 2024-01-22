52f55bd4a855418e371757c12ed0ac89f32494c6
Tri Hargianto <trihargianto@gmail.com>
2024-01-22 11:40:00 +0700

    tab to spaces & use jj keymap

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- EDITING
+------------------------------------------------------------------------
+
+-- Indents will have a width of 4 and use spaces instead of tabs
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
+-- Key mappings
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true })
