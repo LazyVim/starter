36fdc6d62cbe7d67700a5011c11cc3a434e4f13a
Brian Koech <brkoech@microsoft.com>
2024-02-08 19:15:24 +0300

    Add neovide configs and shell

---
+++ lua/config/neovide.lua
@@ -0,0 +1,18 @@
+if vim.g.neovide then
+  vim.o.guifont = "FiraCode Nerd Font:h12"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_opacity = 200
+  vim.g.neovide_cursor_vfx_particle_lifetime = 0.1
+  vim.g.neovide_cursor_vfx_particle_density = 10
+  vim.g.neovide_cursor_vfx_particle_speed = 10
+  vim.g.neovide_cursor_vfx_particle_length = 0.2
+  vim.g.neovide_cursor_vfx_particle_offset = 0
+  vim.g.neovide_cursor_vfx_color_map = "rainbow"
+  vim.g.neovide_cursor_vfx_enable = true
+  vim.g.neovide_cursor_vfx_inertia = 0.1
+  vim.g.neovide_cursor_vfx_max_particles = 1000
+
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_refresh_rate = 60
+  -- vim.g.neovide_theme = "tokyonight"
+end
