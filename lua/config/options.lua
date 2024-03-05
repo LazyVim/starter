31cf6303789acf9c3cd1c847ed32e5478a19163d
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-03-05 12:13:57 +0100

    expand tab false

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+vim.opt.expandtab = false -- Use spaces instead of tabs
