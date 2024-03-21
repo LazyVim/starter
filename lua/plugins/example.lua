f035949eb8dc21fa71e86cbeb1c89c4ed9c01d0d
Tomasz Mysliwiec <tmysl@me.com>
2024-03-21 10:11:31 -0400

    Update nvm-telescope-fzf build command
    Update the build command for nvm-telescope-fzf to match upstream docs and configs documented in https://github.com/nvim-telescope/telescope-fzf-native.nvim#installation docs

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -76,7 +76,7 @@ return {
     "telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
       config = function()
         require("telescope").load_extension("fzf")
       end,
