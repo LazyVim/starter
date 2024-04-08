3105169ead4ba5c8257cefe65947ea56ac1a735b
AceAiAid <152113284+AceAiAid@users.noreply.github.com>
2024-04-08 11:21:15 -0700

    options: show file status in window bar (#2)
    Co-authored-by: aceaiaid <“aceaiaid@gmail.com”>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- show file status in window bar
+vim.opt.winbar = "%=%m %f"
