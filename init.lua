99c333eb2b0b5872a514a17fe8cb007a06ac6f20
aceaiaid <aceaiaid@gmail.com>
2024-04-08 10:42:08 -0700

    feat: copy from WSL2 ubuntu to Windows clipboard

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- to copy from WSL2 ubuntu to Windows
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "/mnt/d/64/win32yank.exe -i --crlf",
+    ["*"] = "/mnt/d/64/win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "/mnt/d/64/win32yank.exe -o --lf",
+    ["*"] = "/mnt/d/64/win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
