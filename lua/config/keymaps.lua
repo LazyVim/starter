add724be2adbad5d10e03376409ad500108cbc93
Kelvin Oenning <kelvinstang@hotmail.com>
2024-04-17 08:46:56 -0300

    feat: remove split screen maps to use default maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local keymap = vim.keymap
-local defaultOpts = { noremap = true, silent = true }
-
--- -- Split Window
-keymap.set("n", "ss", ":split<Return>", defaultOpts)
-keymap.set("n", "sv", ":vsplit<Return>", defaultOpts)
