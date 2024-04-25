be84eb5e6c9b6c9e75f44b147bace4ede4b708e9
Maxim Egorov <13maximegorov@gmail.com>
2024-04-25 13:21:49 +0500

    fix

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,8 +8,10 @@ return {
   },
   {
     "folke/noice.nvim",
-    opts = function(_, opts)
-      opts.presets.lsp_doc_border = true
-    end,
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
   },
 }
