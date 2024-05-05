c21e6a58a8705729b6bf704519138245e718d59b
MattSchaab <schaab.matias@gmail.com>
2024-05-05 11:23:20 -0300

    Cleaning IDE

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "noice.nvim", enabled = false },
+  { "nui.nvim", enabled = false },
 }
