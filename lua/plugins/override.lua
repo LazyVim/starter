4a8ddf5245e35bf4d4e69d7af0e1b1e9b462f224
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-13 13:29:37 +0700

    chore: manage plugin based on purpose

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -1,22 +1,8 @@
-local opt = vim.opt
-opt.tabstop = 4
-opt.smartindent = true
-opt.shiftwidth = 4
-opt.expandtab = true
 return {
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "everforest",
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      tabstop = 4,
-      smartindent = true,
-      shiftwidth = 4,
-      expandtab = true,
     },
   },
 }
