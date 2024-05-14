04a86beaab3a327ffe0159d66e88b63595a51d8b
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-14 07:16:39 +0700

    feat: map with F keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,12 @@ map({ "n", "v" }, ";", ":", { desc = "Enter command mode", noremap = true, nowai
 
 -- File management Nvim
 map("n", "<leader>op", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+map({ "n", "i", "v" }, "<f12>", function()
+  vim.lsp.buf.definition()
+end, { desc = "Go to definition" })
+map({ "n", "i", "v" }, "<F12>", function()
+  vim.lsp.buf.declaration()
+end, { desc = "Go to declaration" })
 
 -- Markdown configs
 map("n", "<leader>ct", ":lua require('toggle-checkbox').toggle()<CR>", { desc = "Toggle Checkbox" })
