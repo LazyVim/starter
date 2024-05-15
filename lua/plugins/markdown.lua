c11cdbf6411e2cb4ca92738315b8dfff33ff134f
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-15 15:33:46 +0700

    feat: add image.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,4 +4,18 @@ return {
     lazy = true,
     event = "VeryLazy",
   },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
+    },
+  },
+  {
+    "3rd/image.nvim",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      -- ...
+    end,
+  },
 }
