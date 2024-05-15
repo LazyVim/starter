c3937ac4a9bb8cf13f24eb7eb9c1246fe62df924
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-15 09:04:27 +0700

    feat: add refactor

---
+++ lua/plugins/development.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "ThePrimeagen/refactoring.nvim",
+    lazy = true,
+    cmd = "Refactor",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+}
