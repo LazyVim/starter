aafe40ece1f41c8d42ee7f73df6b79f16329dda7
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-15 15:33:07 +0700

    feat: disable auto check for update
    - due to slow start up, gonna disable check for update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,8 +27,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  checker = { enabled = true }, -- automatically check for plugin updates
   install = { colorscheme = { "everforest" } },
+  checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
