1ed3febfc603269381a07e37017e398d1c29b858
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-15 15:32:09 +0700

    feat: change default color scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,8 +27,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
+  install = { colorscheme = { "everforest" } },
   performance = {
     rtp = {
       -- disable some rtp plugins
