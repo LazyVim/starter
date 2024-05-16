417c2a2d44e868e503a39bb7fc8d9b814a5a8a9c
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-16 09:50:17 +0700

    feat: add flutter tools

--- lua/plugins/development.lua
+++ lua/plugins/development.lua
@@ -8,4 +8,13 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
   },
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = true,
+    ft = "dart",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+  },
 }
