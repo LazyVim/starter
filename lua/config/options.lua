35537dfa7bc81c6f235a20d19dbed5ebe35cd6f2
Raffaele Wylde <raffaelewylde@gmail.com>
2024-05-15 21:06:11 -0400

    added some personal options and a plugin for kitty config file syntax highlighting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakat = " ^I!@*-+;:,./?"
+vim.opt.showbreak = "++"
+vim.g.ruby_host_prog = "/Users/wylde/.gem/ruby/3.3.0/bin/neovim-ruby-host"
---
+++ lua/plugins/vim-kitty.lua
@@ -0,0 +1,3 @@
+return {
+  "fladson/vim-kitty",
+}
