f7f202db049c96c835971fd4ce661542905957e3
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-16 12:44:43 +0700

    feat: remove image.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,18 +4,4 @@ return {
     lazy = true,
     ft = "markdown",
   },
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1001, -- this plugin needs to run before anything else
-    opts = {
-      rocks = { "magick" },
-    },
-  },
-  {
-    "3rd/image.nvim",
-    lazy = true,
-    event = "VeryLazy",
-    dependencies = { "luarocks.nvim" },
-    opts = {},
-  },
 }
