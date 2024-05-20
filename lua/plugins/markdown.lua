c56d076b0ce110f27b0dec5cb3c1f13ade5db325
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-20 22:56:12 +0700

    feat: add otter

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -13,4 +13,14 @@ return {
       dir = "~/Workspace/Notes/",
     },
   },
+  {
+    "jmbuhr/otter.nvim",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "hrsh7th/nvim-cmp", -- optional, for completion
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
