396dd843ad6db229dec0aa8f4e69979444a2bade
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-21 11:19:57 +0700

    feat: config CodeSnap

--- lua/plugins/collaborative.lua
+++ lua/plugins/collaborative.lua
@@ -15,6 +15,10 @@ return {
     },
     opts = {
       save_path = "~/Pictures/ScreenShot/",
+      has_line_number = true,
+      watermark = "• FM39hz •",
+      bg_color = "#535c68",
+      code_font_family = "JetbrainsMono Nerd Font Bold",
     },
   },
 }
