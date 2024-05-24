d77f82e2b5b6d236ce24f71fe40ec804a3d46727
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-24 15:50:46 +0700

    feat: add rounded corner for mini.files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesWindowOpen",
+  callback = function(args)
+    local win_id = args.data.win_id
+    vim.api.nvim_win_set_config(win_id, { border = "rounded" })
+  end,
+})
