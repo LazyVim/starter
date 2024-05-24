abc7a88d12fbe6a0e96cd0ba35baa50976a13d2d
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-24 15:32:51 +0700

    feat: switch from lazyterm to toggleterm

--- lua/plugins/file-management.lua
+++ lua/plugins/file-management.lua
@@ -5,4 +5,23 @@ return {
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+  {
+    "akinsho/toggleterm.nvim",
+    cmd = "ToggleTerm",
+    opts = {
+      float_opts = {
+        border = "curved",
+        title_pos = "left",
+        width = 128,
+        height = 16,
+        row = 1,
+      },
+      winbar = {
+        enabled = true,
+        name_formatter = function(term) --  term: Terminal
+          return term.name
+        end,
+      },
+    },
+  },
 }
