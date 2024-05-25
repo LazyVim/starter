c60085d88029f80d46d5db9ba2718a02ca588d8a
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-25 21:05:37 +0700

    feat: config eslint

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -68,4 +68,21 @@ return {
       close_if_last_window = true,
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
 }
