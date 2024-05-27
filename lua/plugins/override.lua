abe5a687016376188a96d2cb873c8376c6013bfa
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-28 01:07:00 +0700

    feat: add L and H keymap

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -74,6 +74,33 @@ return {
 		opts = {
 			auto_clean_after_session_restore = true,
 			close_if_last_window = true,
+			window = {
+				width = 30,
+				mappings = {
+					["h"] = function(state)
+						local node = state.tree:get_node()
+						if node.type == "directory" and node:is_expanded() then
+							require("neo-tree.sources.filesystem").toggle_directory(state, node)
+						else
+							require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+						end
+					end,
+					["l"] = function(state)
+						local node = state.tree:get_node()
+						local path = node:get_id()
+						if node.type == "directory" then
+							if not node:is_expanded() then
+								require("neo-tree.sources.filesystem").toggle_directory(state, node)
+							elseif node:has_children() then
+								require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+							end
+						end
+						if node.type == "file" then
+							require("neo-tree.utils").open_file(state, path)
+						end
+					end,
+				},
+			},
 		},
 	},
 	{
