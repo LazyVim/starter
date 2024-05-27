3b779c75d8b6fee6eed2bdd0dc9524fbb75a4105
Raffaele Wylde <raffaelewylde@gmail.com>
2024-05-27 14:41:00 -0400

    added keymaps for jj as ESC and leader-W as write

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, desc = "Save Window" })
