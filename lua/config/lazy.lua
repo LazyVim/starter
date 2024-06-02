3f061734eda4d890daa116ffea5794ec28e74bcb
DrummyFloyd <jonathan.monnet28@gmail.com>
2024-06-02 16:40:16 +0200

    fix: removed unnecessary env var
    according to https://github.com/LazyVim/LazyVim/issues/2063#issuecomment-2143841592
    this is not needed

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
