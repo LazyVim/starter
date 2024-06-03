2c5786f3eb0ea84c093857284bae9b5362f74225
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-04 00:42:02 +0700

    feat: use animation for notify

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -125,19 +125,13 @@ return {
 		},
 	},
 	{
-		"neovim/nvim-lspconfig",
+		"rcarriga/nvim-notify",
 		opts = {
-			servers = { eslint = {} },
-			setup = {
-				eslint = function()
-					require("lazyvim.util").lsp.on_attach(function(client)
-						if client.name == "eslint" then
-							client.server_capabilities.documentFormattingProvider = true
-						elseif client.name == "tsserver" then
-							client.server_capabilities.documentFormattingProvider = false
-						end
-					end)
-				end,
+			timeout = 2500,
+			fps = 170,
+			stages = "fade_in_slide_out",
+		},
+	},
 			},
 		},
 	},
