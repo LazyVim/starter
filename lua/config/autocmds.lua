9d2876b140e77b6ed15cf1e0a1c094a0c0ec0446
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-04 00:45:06 +0700

    doc: add documentation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set Mini.files rounded border
 vim.api.nvim_create_autocmd("User", {
 	pattern = "MiniFilesWindowOpen",
 	callback = function(args)
