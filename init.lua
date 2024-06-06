ccc2c83bae12a332afeec661d6468518b305645a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-06 16:50:02 -0300

    Setup ruby LSP

--- init.lua
+++ init.lua
@@ -1,2 +1,28 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- init.lua
+local lspconfig = require("lspconfig")
+
+-- Setup Ruby Language Server
+lspconfig.ruby_ls.setup({
+  cmd = { "ruby-lsp" },
+  filetypes = { "ruby" },
+  settings = {
+    ruby = {
+      diagnostics = true,
+      completion = true,
+    },
+  },
+})
+
+-- Setup Solargraph
+lspconfig.solargraph.setup({
+  cmd = { "solargraph", "stdio" },
+  filetypes = { "ruby" },
+  settings = {
+    solargraph = {
+      diagnostics = true,
+    },
+  },
+})
