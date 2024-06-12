69aa48cd2f4d8ebf22c44e8ab68e6b3c364a6aac
Roman Frołow <rofrol@gmail.com>
2024-06-12 18:21:28 +0200

    Use unpack

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -157,19 +157,15 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     config = function(_, opts)
-      table.insert(opts.sections.lualine_a,
-        function()
-          return "😄"
-        end
-      )
-
-      -- prepend
-      table.insert(opts.sections.lualine_z, 1, "location")
-
       require('lualine').setup {
         sections = {
-          lualine_a = opts.sections.lualine_a,
-          lualine_z = opts.sections.lualine_z,
+          lualine_a = function()
+            return "😄"
+          end,
+          lualine_z = {
+            "location",
+            unpack(opts.sections.lualine_z),
+          },
         }
       }
     end,
