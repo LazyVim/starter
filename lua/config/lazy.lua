704529dfc58bacbd93ab06c262a995532af55f17
Aaron John <unrealapex@disroot.org>
2024-06-11 21:53:55 -0700

    tweak: use default colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight", "default" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
