30a63d33c0030b38071eda0f6ef4bd5ecf352d62
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-20 16:48:33 +0700

    feat: add netman

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -93,6 +93,12 @@ return {
 		opts = {
 			auto_clean_after_session_restore = true,
 			close_if_last_window = true,
+			sources = {
+				"filesystem",
+				"buffers",
+				"git_status",
+				"netman.ui.neo-tree",
+			},
 			window = {
 				width = 30,
 				mappings = {
