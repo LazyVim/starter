e873c00695cfaae12c3c872e7712c29434669783
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-27 14:11:20 +0700

    feat: change nvim-dap layout

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -203,4 +203,36 @@ return {
 			},
 		},
 	},
+	{
+		"rcarriga/nvim-dap-ui",
+		opts = {
+			expand_lines = true,
+			icons = { expanded = "", collapsed = "", circular = "" },
+			layouts = {
+				{
+					elements = {
+						{ id = "watches", size = 0.24 },
+						{ id = "scopes", size = 0.24 },
+						{ id = "breakpoints", size = 0.24 },
+						{ id = "stacks", size = 0.28 },
+					},
+					size = 0.23,
+					position = "right",
+				},
+				{
+					elements = {
+						{ id = "repl", size = 0.55 },
+						{ id = "console", size = 0.45 },
+					},
+					size = 0.27,
+					position = "bottom",
+				},
+			},
+			floating = {
+				max_height = 0.9,
+				max_width = 0.5,
+				border = "rounded",
+			},
+		},
+	},
 }
