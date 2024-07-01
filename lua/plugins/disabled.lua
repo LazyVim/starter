a8cb988327197dd7a2b11c6a5bd7439de761574e
MattSchaab <schaab.matias@gmail.com>
2024-06-30 22:45:03 -0300

    Update to some plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "noice.nvim", enabled = false },
   { "nui.nvim", enabled = false },
+  -- This one selects searches
+  { "flash.nvim", enabled = false },
 }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>/", function()
+  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+    previewer = false,
+  }))
+end, { desc = "[/] Fuzzily search in current buffer" })
+return {}
