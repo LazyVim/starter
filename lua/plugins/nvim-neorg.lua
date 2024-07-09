7c0c5f430f1542bd4df8ca9334e9bf020588b4d4
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-07-09 02:34:09 -0400

    added nvim-neorg plugin config

---
+++ lua/plugins/nvim-neorg.lua
@@ -0,0 +1,6 @@
+return {
+  "nvim-neorg/neorg",
+  lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  version = "8.9.0", -- Pin Neorg to the latest stable release
+  config = true,
+}
