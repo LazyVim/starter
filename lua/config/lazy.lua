de458a2ac480f0f0d8eee768cb2555e3b0926020
Joao Pedro Veloso <JPV@MacBook-Pro.lan>
2024-07-10 22:48:07 -0500

    changing colorscheme to rose-pine

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "rose-pine" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
