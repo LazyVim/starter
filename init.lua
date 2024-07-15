e30113f41b270d60aa4097ee52ad8de18f0f0084
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-07-15 13:53:44 +0700

    feat: implement neovide visual

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require("config.lazy")
 
 if vim.g.neovide then
-	vim.o.guifont = "JetBrainsMono Nerd Font:h16:b"
+	vim.o.guifont = "JetBrainsMono_Nerd_Font:h15.6:w0.8:b"
+	vim.opt.linespace = -4
 	vim.g.neovide_refresh_rate = 170
 end
