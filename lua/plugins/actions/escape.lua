15dec0f5af1830f3f917d175e29813b9f66b4386
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-07-16 11:57:52 +0700

    fix: fix better escape in lazygit and terminal

--- lua/plugins/actions/escape.lua
+++ lua/plugins/actions/escape.lua
@@ -1,5 +1,11 @@
 return {
-  "max397574/better-escape.nvim",
-  event = "InsertEnter",
-  opts = {},
+	"max397574/better-escape.nvim",
+	event = "InsertEnter",
+	opts = {
+		default_mappings = true,
+		mappings = {
+			t = { j = { false } }, --lazygit navigation fix
+			v = { j = { k = false } }, -- visual select fix
+		},
+	},
 }
