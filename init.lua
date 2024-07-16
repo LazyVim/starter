c3bab0cb7c94862c89b440136410204a06536a2a
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-07-16 13:38:08 +0700

    chore: configure font for neovide

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
 if vim.g.neovide then
-	vim.o.guifont = "JetBrainsMono_Nerd_Font:h15.6:w0.8:b"
+	vim.o.guifont = "JetBrainsMono Nerd Font:h15.4:b:i"
 	vim.opt.linespace = -4
 	vim.g.neovide_refresh_rate = 170
 end
