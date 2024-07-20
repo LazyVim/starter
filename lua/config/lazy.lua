cb6349c8ae922d1c5745574f4d17b44f2731d451
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-20 23:30:46 +0200

    fix: disable lazy checker notify by default

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,10 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
