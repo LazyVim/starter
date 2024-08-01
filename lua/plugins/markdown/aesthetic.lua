fc06a40a99abeccbf0ea1d079e99aa433fb541d8
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-08-01 09:44:56 +0700

    chore: disable markview as it conflict with markdown

--- lua/plugins/markdown/aesthetic.lua
+++ lua/plugins/markdown/aesthetic.lua
@@ -1,10 +1,10 @@
 return {
-  "OXY2DEV/markview.nvim",
-  ft = "markdown",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("markview").setup()
-  end,
+  -- "OXY2DEV/markview.nvim",
+  -- ft = "markdown",
+  -- dependencies = {
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
+  -- config = function()
+  --   require("markview").setup()
+  -- end,
 }
