830038c694d24d7d7f3185d5310d46e81531e7b3
Minh Quang Bui <minhquangbui053@gmail.com>
2024-09-08 23:22:42 +0700

    Add vim-plug + vim-fugitive

---
+++ init.vim
@@ -0,0 +1,5 @@
+call plug#begin('~/.vim/plugged')
+Plug 'tpope/vim-fugitive'
+call plug#end()
+
+lua require('config')
--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 
 sudo apt-get install ripgrep
 
+# setup font
 mkdir -p ~/.local/share/fonts
 cp .font/{*.ttf,*.otf} ~/.local/share/fonts
 sudo cp *.ttf /usr/share/fonts/truetype/
 sudo fc-cache -f -v
+
+# setup vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
