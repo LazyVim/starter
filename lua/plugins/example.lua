9a7552b84c46a10fbb609f2bdeefc662a2339e05
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2024-10-05 22:13:30 +0300

    docs: fix `lualine` component example
    As per https://github.com/LazyVim/LazyVim/issues/4544 it creates
    confusion to the users who are just trying out the example to try things
    out. Better to avoid such misinterpretations in the future.
    
    I'm assuming this will also update the docs automatically as I can
    deduce from
    https://github.com/LazyVim/lazyvim.github.io/blob/25af26046a30be110f0aa19c87ad2a1a1e53ce45/lua/build.lua#L369-L372?

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -157,7 +157,11 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
+      table.insert(opts.sections.lualine_x, {
+        function()
+          return "😄"
+        end,
+      })
     end,
   },
 
