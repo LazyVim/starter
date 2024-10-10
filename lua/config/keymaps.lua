323f1a7057e88ec8881ec10d6d101fc4e2aae1de
Omar Mendoza <1206997+omaryahir@users.noreply.github.com>
2024-10-10 00:20:33 -0600

    feat: moving s standard behavior and fix for relative number [custom-2024-10-10]

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.opt.relativenumber = false
