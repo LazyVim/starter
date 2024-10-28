dab942d7e58faf7d45c91f98ee4f3534755525bf
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-10-28 09:26:47 +0700

    feat: add dependencies for ufo

--- lua/plugins/visualize/fold.lua
+++ lua/plugins/visualize/fold.lua
@@ -1,4 +1,10 @@
 return {
   "kevinhwang91/nvim-ufo",
+  dependencies = "kevinhwang91/promise-async",
   event = "BufReadPre",
+  opts = {
+    provider_selector = function(bufnr, filetype, buftype)
+      return { "treesitter", "indent" }
+    end,
+  },
 }
