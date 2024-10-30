242d4e68ca6db674367c43f00d093ece0b0471b7
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-10-30 13:48:39 +0700

    style: config buffer line

--- lua/plugins/override/visualize.lua
+++ lua/plugins/override/visualize.lua
@@ -13,6 +13,22 @@ return {
 			},
 		},
 	},
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				separator_style = "slope",
+				hover = {
+					enabled = true,
+					delay = 200,
+					reveal = { "close" },
+				},
+				indicator = {
+					style = "underline",
+				},
+			},
+		},
+	},
 	-- {
 	-- 	"nvim-lualine/lualine.nvim",
 	-- 	dependencies = {
